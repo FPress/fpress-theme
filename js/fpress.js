8574c239e4d077a85d4ac7163db5cea6e2930fbb
ollieestuff <spiele2002@hotmail.com>
2018-01-01 20:56:10 -0200

    navbar update now dynamic and responsive

--- js/fpress.js
+++ js/fpress.js
@@ -3,6 +3,9 @@ jQuery(document).ready(function($) {
 
 
    $(".menu").click(function(){
+      $('body,html').animate({
+       scrollTop: 0
+    }, 300);
       $(".navbar").slideToggle(300);
    });
 
